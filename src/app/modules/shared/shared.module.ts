903a54d3889bd754b8c414a6968ef81f2d9906c9
nlara <noe_lara99@outlook.com>
2024-03-30 15:52:07 -0600

    feat: :sparkles: Header, Footer, Profile components

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 
 @NgModule({
   declarations: [
-    SideNavComponent
+    SideNavComponent,
+    LogoComponent,
+    MenuComponent,
+    FooterComponent,
+    HeaderComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule
